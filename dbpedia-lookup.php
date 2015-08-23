8ed8914f56f68eac69fd83416fed1eed6b5de953
Akhil Ramachandran <akhilram@usc.edu>
2015-08-23 13:30:00 -0700

    all fields added, all cities added

--- dbpedia-lookup.php
+++ dbpedia-lookup.php
@@ -5,16 +5,29 @@
 $jsonurl = 'http://dbpedia.org/data/' . $_GET['city'] . '.json';
 $json = file_get_contents($jsonurl);
 
+
+
+$resourceKey = 'http://dbpedia.org/resource/' . $_GET['city'];
+$populationTotalKey = 'http://dbpedia.org/ontology/populationTotal';
+$populationDensityKey = 'http://dbpedia.org/ontology/populationDensity';
+$areaTotalKey = 'http://dbpedia.org/ontology/areaTotal';
+
 $json = json_decode($json);
 // $json_city = json_decode($json['http://dbpedia.org/resource/Los_Angeles'])
 
 $cityInfo = [];
 
 foreach ($json as $key => $value) {
-	if($key == 'http://dbpedia.org/resource/Los_Angeles') {
+	if($key == $resourceKey) {
 		foreach ($value as $key1 => $value1) {
-			if ($key1 == 'http://dbpedia.org/ontology/populationTotal') {
+			if ($key1 == $populationTotalKey) {
 				$cityInfo["populationTotal"] = $value1[0]->value;
+			}
+			if ($key1 == $populationDensityKey) {
+				$cityInfo["populationDensity"] = $value1[0]->value;
+			}
+			if ($key1 == $areaTotalKey) {
+				$cityInfo["areaTotal"] = $value1[0]->value;
 			}
 		}
 		// $value = json_decode($value);
