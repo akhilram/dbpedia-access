bcc79ac5e79c70ab8d235bcc9be3e83fcbfe6279
Akhil Ramachandran <akhilram@usc.edu>
2015-08-23 13:16:57 -0700

    individual field fetched

--- dbpedia-lookup.php
+++ dbpedia-lookup.php
@@ -6,13 +6,23 @@ $jsonurl = 'http://dbpedia.org/data/' . $_GET['city'] . '.json';
 $json = file_get_contents($jsonurl);
 
 $json = json_decode($json);
+// $json_city = json_decode($json['http://dbpedia.org/resource/Los_Angeles'])
+
+$cityInfo = [];
 
 foreach ($json as $key => $value) {
-	if ($key == 'http://dbpedia.org/ontology/populationTotal') {
-		echo $key . ' ';
+	if($key == 'http://dbpedia.org/resource/Los_Angeles') {
+		foreach ($value as $key1 => $value1) {
+			if ($key1 == 'http://dbpedia.org/ontology/populationTotal') {
+				$cityInfo["populationTotal"] = $value1[0]->value;
+			}
+		}
+		// $value = json_decode($value);
+		
 	}
 	
 }
-// var_dump($json);
+echo json_encode($cityInfo);
+// var_dump($json_city);
 
 ?>
