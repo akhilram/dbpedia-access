fd1e38a4b3f59a4e2853d0cc2b5a67687dddeb39
Akhil Ramachandran <akhilram@usc.edu>
2015-08-23 18:37:03 -0700

    spinner added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ $( document ).ready(function() {
 });
 
 function getJSONData(e) {
-
+  $("#result").css("display", "none");
+  addSpinner();
   var url = "dbpedia-lookup.php";
   var data = {};
   data["city"] = $("#city option:selected").val();
@@ -23,9 +24,38 @@ function getJSONData(e) {
 }
 
 function processData(output) {
+  $("#result").css("display", "inline");
+  spinner.stop();
   $("#collapse").collapse('show');
   $("#area").text(output["areaTotal"]);
   $("#population").text(output["populationTotal"]);
   $("#density").text(output["populationDensity"]);
   console.log(output);
+}
+
+function addSpinner() {
+  var opts = {
+    lines: 13,
+    length: 28,
+    width: 14,
+    radius: 42,
+    scale: 1, 
+    corners: 1, 
+    color: '#000', 
+    opacity: 0.35, 
+    rotate: 0, 
+    direction: 1, 
+    speed: 1, 
+    trail: 60, 
+    fps: 20, 
+    zIndex: 2e9, 
+    className: 'spinner', 
+    top: '50%', 
+    left: '50%', 
+    shadow: false, 
+    hwaccel: false, 
+    position: 'absolute' 
+  }
+  var target = document.getElementById('spinner')
+  spinner = new Spinner(opts).spin(target);
 }
