1d2cd6d5949504e6b0509fa4c9c5dd0c317dec92
Akhil Ramachandran <akhilram@usc.edu>
2015-08-23 17:23:18 -0700

    jquery logic added

--- dbpedia-lookup.php
+++ dbpedia-lookup.php
@@ -1,41 +1,31 @@
 <?php
 
-// echo $_GET['city'];
+$jsonURL 	= 'http://dbpedia.org/data/' . $_GET['city'] . '.json';
+$jsonData 	= file_get_contents($jsonURL);
+$jsonArray 	= json_decode($jsonData, true);
 
-$jsonurl = 'http://dbpedia.org/data/' . $_GET['city'] . '.json';
-$json = file_get_contents($jsonurl);
-
-
-
-$resourceKey = 'http://dbpedia.org/resource/' . $_GET['city'];
-$populationTotalKey = 'http://dbpedia.org/ontology/populationTotal';
-$populationDensityKey = 'http://dbpedia.org/ontology/populationDensity';
-$areaTotalKey = 'http://dbpedia.org/ontology/areaTotal';
-
-$json = json_decode($json);
-// $json_city = json_decode($json['http://dbpedia.org/resource/Los_Angeles'])
+$resourceKey 			= 'http://dbpedia.org/resource/' . $_GET['city'];
+$populationTotalKey 	= 'http://dbpedia.org/ontology/populationTotal';
+$populationDensityKey 	= 'http://dbpedia.org/ontology/populationDensity';
+$areaTotalKey 			= 'http://dbpedia.org/ontology/areaTotal';
 
 $cityInfo = [];
+$cityInfo["populationTotal"] 	= $jsonArray[$resourceKey][$populationTotalKey][0]['value'];
+$cityInfo["populationDensity"] 	= $jsonArray[$resourceKey][$populationDensityKey][0]['value'];
+$cityInfo["areaTotal"] 			= $jsonArray[$resourceKey][$areaTotalKey][0]['value'];
 
-foreach ($json as $key => $value) {
-	if($key == $resourceKey) {
-		foreach ($value as $key1 => $value1) {
-			if ($key1 == $populationTotalKey) {
-				$cityInfo["populationTotal"] = $value1[0]->value;
-			}
-			if ($key1 == $populationDensityKey) {
-				$cityInfo["populationDensity"] = $value1[0]->value;
-			}
-			if ($key1 == $areaTotalKey) {
-				$cityInfo["areaTotal"] = $value1[0]->value;
-			}
-		}
-		// $value = json_decode($value);
-		
-	}
-	
-}
 echo json_encode($cityInfo);
-// var_dump($json_city);
+
+function getJSONURL() {
+	return 'http://dbpedia.org/data/' . $_GET['city'] . '.json';
+}
+
+function getJSONData($jsonURL) {
+	return file_get_contents($jsonURL);
+}
+
+function getAssociativeArray($jsonData) {
+	return json_decode($jsonData);
+}
 
 ?>
